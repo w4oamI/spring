845554923bf1b0a1544b4507eba3f272ae378fbf
w4oamI <ppas1515@naver.com>
2022-01-17 22:54:57 +0900

    깃커밋 테스

--- BoardWeb/src/main/java/com/spring/biz/HomeController.java
+++ BoardWeb/src/main/java/com/spring/biz/HomeController.java
@@ -19,9 +19,7 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	//깃 커밋 테스트
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
