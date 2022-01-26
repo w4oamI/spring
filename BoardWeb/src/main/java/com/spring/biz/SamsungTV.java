5365261d0355b0b6a1b1939089cd5a926870716a
w4oamI <ppas1515@naver.com>
2022-01-26 21:50:28 +0900

    setter 인젝션 사용

--- BoardWeb/src/main/java/com/spring/biz/SamsungTV.java
+++ BoardWeb/src/main/java/com/spring/biz/SamsungTV.java
@@ -24,14 +24,22 @@ public class SamsungTV implements TV{
 	public SamsungTV() {
 		System.out.println("SamsungTV 객체를 생성합니다.");
 	}
-	//생성자 추가 
-	public SamsungTV(Speaker speaker) {
-		System.out.println("SamsungTV(2)객체생성 ");
-		this.speaker = speaker;
+//	//생성자 추가 
+//	public SamsungTV(Speaker speaker) {
+//		System.out.println("SamsungTV(2)객체생성 ");
+//		this.speaker = speaker;
+//	}
+//	public SamsungTV(Speaker speaker, int price) {
+//		System.out.println("SamsungTV(3)객체생성 ");
+//		this.speaker = speaker;
+//		this.price = price;
+//	}
+	public void setSpeaker(Speaker speaker) {
+		System.out.println("setSpeaker 호출");
+		this.speaker=speaker;
 	}
-	public SamsungTV(Speaker speaker, int price) {
-		System.out.println("SamsungTV(3)객체생성 ");
-		this.speaker = speaker;
+	public void setPrice(int price) {
+		System.out.println("setPrice 호출");
 		this.price = price;
 	}
 	public void powerOn() {
