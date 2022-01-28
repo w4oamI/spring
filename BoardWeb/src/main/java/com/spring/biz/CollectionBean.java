5e4df29a8fd190b01091f0b5d5d8d36061e143c4
w4oamI <ppas1515@naver.com>
2022-01-28 20:41:24 +0900

    컬렉션 객체 설정 -Set타입 맵

--- BoardWeb/src/main/java/com/spring/biz/CollectionBean.java
+++ BoardWeb/src/main/java/com/spring/biz/CollectionBean.java
@@ -1,14 +1,22 @@
 package com.spring.biz;
 
 import java.util.List;
+import java.util.Set;
 
 public class CollectionBean {
-	private List<String> addressList;
+	//List 타입맵
+//	private List<String> addressList;
+//	
+//	public void setAddressList(List<String> addressList) {
+//		this.addressList = addressList;
+//	}
+//	public List<String> getAddressList(){
+//		return addressList;
+//	}
 	
-	public void setAddressList(List<String> addressList) {
+	private Set<String> addressList;
+	
+	public void setAddressList(Set<String> addressList) {
 		this.addressList = addressList;
-	}
-	public List<String> getAddressList(){
-		return addressList;
 	}
 }
