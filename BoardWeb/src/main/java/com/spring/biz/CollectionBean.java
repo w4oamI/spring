b951d1a52236c7c7c6cb96cb54b67d62a309e058
w4oamI <ppas1515@naver.com>
2022-01-28 20:30:44 +0900

    List 타입 맵핑 추가 코

---
+++ BoardWeb/src/main/java/com/spring/biz/CollectionBean.java
@@ -0,0 +1,14 @@
+package com.spring.biz;
+
+import java.util.List;
+
+public class CollectionBean {
+	private List<String> addressList;
+	
+	public void setAddressList(List<String> addressList) {
+		this.addressList = addressList;
+	}
+	public List<String> getAddressList(){
+		return addressList;
+	}
+}
--- BoardWeb/src/main/java/com/spring/biz/SamsungTV.java
+++ BoardWeb/src/main/java/com/spring/biz/SamsungTV.java
@@ -24,16 +24,16 @@ public class SamsungTV implements TV{
 	public SamsungTV() {
 		System.out.println("SamsungTV 객체를 생성합니다.");
 	}
-//	//생성자 추가 
-//	public SamsungTV(Speaker speaker) {
-//		System.out.println("SamsungTV(2)객체생성 ");
-//		this.speaker = speaker;
-//	}
-//	public SamsungTV(Speaker speaker, int price) {
-//		System.out.println("SamsungTV(3)객체생성 ");
-//		this.speaker = speaker;
-//		this.price = price;
-//	}
+	//생성자 추가 
+	public SamsungTV(Speaker speaker) {
+		System.out.println("SamsungTV(2)객체생성 ");
+		this.speaker = speaker;
+	}
+	public SamsungTV(Speaker speaker, int price) {
+		System.out.println("SamsungTV(3)객체생성 ");
+		this.speaker = speaker;
+		this.price = price;
+	}
 	public void setSpeaker(Speaker speaker) {
 		System.out.println("setSpeaker 호출");
 		this.speaker=speaker;
