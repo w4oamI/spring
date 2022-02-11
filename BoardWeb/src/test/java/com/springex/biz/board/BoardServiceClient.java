4ad31fbdd355e8d2b5bccc12f07ddfed2791e81c
w4oamI <ppas1515@naver.com>
2022-02-11 22:06:12 +0900

    게시판 만들기 (5) -비즈니스 컴포넌트 의존성 주입(기존 어노테이션 -> setter)

--- BoardWeb/src/test/java/com/springex/biz/board/BoardServiceClient.java
+++ BoardWeb/src/test/java/com/springex/biz/board/BoardServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class BoardServiceClient {
 	public static void main(String[] args) {
-	AbstractApplicationContext container = new GenericXmlApplicationContext("applicationContext");
+	AbstractApplicationContext container = new GenericXmlApplicationContext("applicationContext.xml");
 	
 	BoardService boardService = (BoardService) container.getBean("boardService");
 	
