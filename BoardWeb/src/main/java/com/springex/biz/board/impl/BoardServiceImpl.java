9973705993c10640d65e881b4956c8c25fa71919
w4oamI <ppas1515@naver.com>
2022-02-15 22:23:50 +0900

    AOP 이해하기(2)

--- BoardWeb/src/main/java/com/springex/biz/board/impl/BoardServiceImpl.java
+++ BoardWeb/src/main/java/com/springex/biz/board/impl/BoardServiceImpl.java
@@ -14,39 +14,21 @@ import com.springex.biz.common.LogAdvice;
 public class BoardServiceImpl implements BoardService{
 	@Autowired
 	public BoardDAO boardDAO;
-//	public LogAdvice log;
-//	
-//	public BoardServiceImpl() {
-//		log = new LogAdvice();
-//	}
-	public Log4jAdvice log;
 	
-	public BoardServiceImpl() {
-		log = new Log4jAdvice();
-	}
 	public void insertBoard(BoardVO vo) {
-//		log.printLog();
-		log.printLogging();
 		boardDAO.insertBoard(vo);
 	}
 	public void deleteBoard(BoardVO vo) {
-//		log.printLog();
-		log.printLogging();
 		boardDAO.deleteBoard(vo);
 	}
 	
 	public void getBoard(BoardVO vo) {
-//		log.printLog();
-		log.printLogging();
 		boardDAO.getBoard(vo);
 	}
 	public void updateBoard(BoardVO vo) {
-//		log.printLog();
-		log.printLogging();
 		boardDAO.updateBoard(vo);
 	}
 	public List<BoardVO> getBoardList(BoardVO vo){
-//		log.printLog();
 		return boardDAO.getBoardList(vo);
 	}
 }
