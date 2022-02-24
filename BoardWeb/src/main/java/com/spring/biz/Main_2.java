8e8f3c6107ec13372410b606f7773b93d41ebfa6
w4oamI <ppas1515@naver.com>
2022-02-24 23:38:57 +0900

    싱글톤패턴 연습2 - 구현

---
+++ BoardWeb/src/main/java/com/spring/biz/Main_2.java
@@ -0,0 +1,11 @@
+package com.spring.biz;
+
+public class Main_2 {
+    public static void main(String[] args) {
+        System.out.println("Start.");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + ":" + TicketMaker.getInstance().getNextTicketNumber());
+        }
+        System.out.println("End.");
+    }
+}
---
+++ BoardWeb/src/main/java/com/spring/biz/TicketMaker.java
@@ -0,0 +1,13 @@
+package com.spring.biz;
+public class TicketMaker {
+    private int ticket = 1000;
+    private static TicketMaker singleton = new TicketMaker();       
+    private TicketMaker() {                                         
+    }                                                               
+    public static TicketMaker getInstance() {                       
+        return singleton;                                           
+    }                                                               
+    public synchronized int getNextTicketNumber() {                 
+        return ticket++;
+    }
+}
