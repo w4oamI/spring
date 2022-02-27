ebceb5291b6332d6659e64a2681fd44ad0ddae01
w4oamI <ppas1515@naver.com>
2022-02-27 21:28:37 +0900

    싱글톤 연습 -싱글톤 구현하기.

--- BoardWeb/src/main/java/com/spring/biz/Main.java
+++ BoardWeb/src/main/java/com/spring/biz/Main.java
@@ -2,14 +2,10 @@ package com.spring.biz;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start.");
-        Singleton obj1 = Singleton.getInstance();
-        Singleton obj2 = Singleton.getInstance();
-        if (obj1 == obj2) {
-            System.out.println("싱글톤 패턴 연습 1 .");
-        } else {
-            System.out.println("싱글톤 패턴 연습 2.");
+        for (int i = 0; i < 9; i++) {
+            Triple triple = Triple.getInstance(i % 3);
+            System.out.println(i + ":" + triple);
         }
         System.out.println("End.");
     }
-
 }
