8ff8b1a5cd5bdedea26db44f41297d2a5e127fbd
w4oamI <ppas1515@naver.com>
2022-03-01 17:07:20 +0900

    복습

--- BoardWeb/src/main/java/com/springex/biz/board/impl/BoardServiceImpl.java
+++ BoardWeb/src/main/java/com/springex/biz/board/impl/BoardServiceImpl.java
@@ -7,28 +7,38 @@ import org.springframework.stereotype.Service;
 
 import com.springex.biz.board.BoardService;
 import com.springex.biz.board.BoardVO;
+import com.springex.biz.common.LogAdvice;
 
 @Service("boardService")
 public class BoardServiceImpl implements BoardService{
 	@Autowired
-	public BoardDAO boardDAO;
+	private BoardDAO boardDAO;
+	private LogAdvice log;
 	
+	public BoardServiceImpl() {
+		log = new LogAdvice();
+	}
 	public void insertBoard(BoardVO vo) {
+		log.printLog();
 		boardDAO.insertBoard(vo);
 	}
 	public void deleteBoard(BoardVO vo) {
+		log.printLog();
 		boardDAO.deleteBoard(vo);
 	}
 	
 	public void updateBoard(BoardVO vo) {
+		log.printLog();
 		boardDAO.updateBoard(vo);
 	}
 	
 	public BoardVO getBoard(BoardVO vo) {
+		log.printLog();
 		return boardDAO.getBoard(vo);
 	}
 	
 	public List<BoardVO> getBoardList(BoardVO vo){
+		log.printLog();
 		return boardDAO.getBoardList(vo);
 	}
 }
