0ae07fd1c384c3a3d057a5c33bfd24b4747cb1a8
w4oamI <ppas1515@naver.com>
2022-03-07 22:27:41 +0900

    After Retuning 어드바이스 After Throwing 어드바이스

---
+++ BoardWeb/src/main/java/com/springex/biz/common/AfterReturning.java
@@ -0,0 +1,7 @@
+package com.springex.biz.common;
+
+public class AfterReturning {
+	public void afterLog() {
+		System.out.println("로직처리 후 동작 출력 ");
+	}
+}
---
+++ BoardWeb/src/main/java/com/springex/biz/common/AfterThowingAdvice.java
@@ -0,0 +1,7 @@
+package com.springex.biz.common;
+
+public class AfterThowingAdvice {
+	public void exceptionLog() {
+		System.out.println("예외처리시 출력 ");
+	}
+}
